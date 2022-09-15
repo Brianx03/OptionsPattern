f6197c2afb4ebf62dd338fbd0dd06b7cf9936f2d
Brian Armenta <brian.armenta@unosquare.com>
2022-09-15 10:11:44 -0500

    project to pratice options pattern

---
+++ Controllers/OptionsTestController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using OptionsPatternNetCore.Models;
+
+namespace OptionsPatternNetCore.Controllers
+{
+    public class OptionsTestController : Controller
+    {
+        private readonly DbConnectionConfiguration _options;
+        private readonly DbConnectionConfiguration _optionsSapshot;
+        private readonly DbConnectionConfiguration _optionsMonitor;
+        public OptionsTestController(IOptions<DbConnectionConfiguration> options, 
+            IOptionsSnapshot<DbConnectionConfiguration> optionsSnapshot,
+            IOptionsMonitor<DbConnectionConfiguration> optionsMonitor)
+        {
+            _options = options.Value;
+            _optionsSapshot = optionsSnapshot.Value;
+            _optionsMonitor = optionsMonitor.CurrentValue;
+        }
+
+        [HttpGet("GetValue")]
+        public IActionResult Index()
+        {
+            var iOptions = $"Values at Application Start:\n" +
+                $"{_options.ToString()}\n" +
+                $"{_options.IpAddress}\n" +
+                $"{_options.User}\n" +
+                $"{_options.Password}\n" +
+                $"\n";
+
+            var iOptionsSnapshot = $"Values at request:\n" +
+                $"{_optionsSapshot.DbManager}\n" +
+                $"{_optionsSapshot.IpAddress}\n" +
+                $"{_optionsSapshot.User}\n" +
+                $"{_optionsSapshot.Password}\n" +
+                $"\n";
+
+            Thread.Sleep(15000);
+
+            var iOptionsMonitor = $"Current Values:\n" +
+                $"{_optionsMonitor.DbManager}\n" +
+                $"{_optionsMonitor.IpAddress}\n" +
+                $"{_optionsMonitor.User}\n" +
+                $"{_optionsMonitor.Password}\n" +
+                $"\n";
+
+
+            return Ok(iOptions + iOptionsSnapshot + iOptionsMonitor);
+        }
+    }
+}
---
+++ Models/DbConnectionConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace OptionsPatternNetCore.Models
+{
+    public class DbConnectionConfiguration
+    {
+        public string DbManager { get; set; }
+        public string IpAddress { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
